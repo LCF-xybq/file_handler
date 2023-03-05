dfa50eaf4e22c2e7a55d3debc23f3409872934b3
LCF-xybq <lcfxybq@163.com>
2023-03-05 18:56:28 +0800

    add load.

--- fileio/__init__.py
+++ fileio/__init__.py
@@ -2,10 +2,11 @@ from .utils import (is_str, is_filepath, has_method,
                     mkdir_or_exist, is_list_of)
 from .handlers import JsonHandler
 from .file_client import FileClient
-from .io import dump
+from .io import dump, load
 
 
 __all__ = [
     'is_str', 'is_filepath', 'has_method', 'mkdir_or_exist',
-    'JsonHandler', 'FileClient', 'is_list_of', 'dump'
+    'JsonHandler', 'FileClient', 'is_list_of', 'dump',
+    'load'
 ]
--- fileio/io.py
+++ fileio/io.py
@@ -6,7 +6,6 @@ from .utils import is_list_of
 from .file_client import FileClient
 from .handlers import BaseFileHandler, JsonHandler
 
-
 FileLikeObject = Union[TextIO, StringIO, BytesIO]
 
 file_handlers = {
@@ -14,7 +13,46 @@ file_handlers = {
 }
 
 
+def load(file: Union[str, Path, FileLikeObject],
+         file_format: Optional[str] = None,
+         file_client_args: Optional[Dict] = None,
+         **kwargs):
+    """Load data from json/.. files.
 
+    Args:
+        file (str or :obj:`Path` or file-like object): Filename or a file-like
+            object.
+        file_format (str, optional): If not specified, the file format will be
+            inferred from the file extension, otherwise use the specified one.
+            Currently supported formats include "json"...
+        file_client_args (dict, optional): Arguments to instantiate a
+            FileClient.
+
+    Returns:
+        The content from the file.
+    """
+    if isinstance(file, Path):
+        file = str(file)
+    if file_format is None and isinstance(file, str):
+        file_format = file.split('.')[-1]
+    if file_format not in file_handlers:
+        raise TypeError(f'Unsupported format: {file_format}')
+
+    handler = file_handlers[file_format]
+    f: FileLikeObject
+    if isinstance(file, str):
+        file_client = FileClient.infer_client(file_client_args, file)
+        if handler.str_like:
+            with StringIO(file_client.get_text(file)) as f:
+                obj = handler.load_from_fileobj(f, **kwargs)
+        else:
+            with BytesIO(file_client.get(file)) as f:
+                obj = handler.load_from_fileobj(f, **kwargs)
+    elif hasattr(file, 'read'):
+        obj = handler.load_from_fileobj(file, **kwargs)
+    else:
+        raise TypeError('"file" must be a filepath str or a file-object')
+    return obj
 
 
 def dump(obj: Any,
